1812c67350207e59e1c38bb113d0659423e1c20b
xcelxlorx <xcelxlorx@gmail.com>
2023-11-07 22:59:26 +0900

    feat: Add random code logic

--- src/main/java/com/gihae/mailserver/mail/MailController.java
+++ src/main/java/com/gihae/mailserver/mail/MailController.java
@@ -1,27 +1,34 @@
 package com.gihae.mailserver.mail;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Slf4j
+@Controller
 @RequiredArgsConstructor
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
 public class MailController {
 
     private final MailService mailService;
 
     //회원가입 시 인증번호 발급
     @PostMapping("/send-verification-code")
-    public ResponseEntity<String> sendCode(){
-        String code = mailService.sendCode();
+    public ResponseEntity<String> sendCode(@RequestParam String email){
+        String code = mailService.sendCode(email);
         return ResponseEntity.ok().body(code);
     }
 
     //임시 비밀번호 발급
     @PostMapping("/send-password")
     public ResponseEntity<String> sendReset(){
-        String password = mailService.sendPassword();
-        return ResponseEntity.ok().body(password);
+        mailService.sendPassword();
+        log.info("send-password");
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/gihae/mailserver/mail/MailService.java
+++ src/main/java/com/gihae/mailserver/mail/MailService.java
@@ -1,17 +1,61 @@
 package com.gihae.mailserver.mail;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class MailService {
 
-    public String sendCode(){
-        return "";
+    private final JavaMailSender javaMailSender;
+
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%";
+    private static final int CODE_LENGTH = 8;
+
+    //인증 번호 발송
+    public String sendCode(String email){
+        String code = createCode();
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email); //메일 수신자
+        message.setSubject("인증 번호입니다."); //메일 제목
+        message.setText("인증 번호 : " + code); //메일 내용
+
+        javaMailSender.send(message);
+        log.info("code=" + code);
+        return code;
     }
 
-    public String sendPassword(){
-        return "";
+    //임시 비밀번호 발송
+    public void sendPassword(){
+    }
+
+    //파일 전송
+    public void sendFile(){
+
+    }
+
+    private String createCode(){
+        try {
+            SecureRandom secureRandom = SecureRandom.getInstanceStrong();
+            StringBuilder code = new StringBuilder();
+
+            for(int i = 0; i <= CODE_LENGTH; i++){
+                int index = secureRandom.nextInt(CHARACTERS.length());
+                char ch = CHARACTERS.charAt(index);
+                code.append(ch);
+            }
+
+            return code.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
